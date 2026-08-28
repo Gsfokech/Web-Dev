3ab8fbe8b10c19c6e58bba6a42946a720aba6b04
Gsfokech <124061229+Gsfokech@users.noreply.github.com>
2026-08-28 23:38:57 +0300

    Add files via upload

---
+++ client/frontend.js
@@ -0,0 +1,62 @@
+const App = {
+    data(){
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.value.trim() && this.form.name.trim()
+        }
+    },
+    methods: {
+        async createContact(){
+            const {...contact} = this.form
+            const newContact = await request('api/contacts', 'POST', contact)
+            this.contacts.push(newContact)
+            this.form.name = this.form.value = ''
+        },
+        async markContact(id){
+            const contact = this.contacts.find(c => c.id === id)
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            })
+            contact.marked = updated.marked
+        },
+        async removeContact(id){
+            await request(`/api/contacts/${id}`, 'DELETE', )
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        }
+    },
+    async mounted(){
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
+    }
+}
+
+async function request(url, method = 'GET', data = null){
+    try{
+        const headers = {}
+        let body
+        if(data){
+            headers['Content-type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        const responce = await fetch (url, {
+            method: method,
+            headers: headers,
+            body: body
+        })
+        return await responce.json()
+    }catch(error){
+        console.warn(`Error: ${error.message}`)
+    }
+}
+
+Vue.createApp(App).mount('#app')
